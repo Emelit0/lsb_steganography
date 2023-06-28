706bc4079d1d03fb9565b496606de2a64cb4f136
Emelit0 <stollk811@gmail.com>
2023-06-28 23:47:19 +0200

    read & write bmp header

--- encode.c
+++ encode.c
@@ -2,6 +2,7 @@
 // fp1 = image file
 // fp2 = secret file
 // fp3 = output file
+#define MAX_MESSAGE_LENGTH 100000000
 
 int size_of_image(FILE *filePath) {
     int width, height;
@@ -31,18 +32,22 @@ int secret_size(FILE *secret) {
     return size;
 }
 
-void hide_secret(const char *inputPath, const char *secretPath, const char *outputPath) {
 
+void readBMPHeader(FILE *filePath, unsigned char *header) {
+    fseek(filePath, 0, SEEK_SET);
+    fread(header, sizeof(unsigned char), 54, filePath);
 };
 
-void readBMPHeader(FILE *filePath) {
-};
-
-void writeBMPHeader(FILE *filePath) {
+void writeBMPHeader(FILE *filePath , FILE *outputFile) {
+    fseek(filePath, 0, SEEK_SET);
+    unsigned char header[54];
+    fread(header, sizeof(unsigned char), 54, filePath);
+    fwrite(header, sizeof(unsigned char), 54, outputFile);
 };
 
 // read a byte from a pixel
 unsigned char readByte(const unsigned char *pixel) {
+
 };
 
 //read a bit from a pixel
@@ -57,6 +62,47 @@ void hideByte(unsigned char *pixel, unsigned char byte) {
 void hideBit(unsigned char *byte, unsigned char bit) {
 };
 
+void hide_secret(const char *inputPath, const char *secretPath, const char *outputPath) {
+    FILE *inputFile = fopen(inputPath, "rb");
+    FILE *secretFile = fopen(secretPath, "r");
+    FILE *outputFile = fopen(outputPath, "wb");
+
+    if (!inputFile || !secretFile || !outputFile) {
+        printf("Error opening files.\n");
+        return;
+    }
+
+    unsigned char header[54];
+    readBMPHeader(inputFile, header);
+    writeBMPHeader(inputFile, outputFile);
+
+    unsigned char pixel[3];
+    int messageIndex = 0;
+    int bitIndex = 0;
+    unsigned char byte = fgetc(secretFile);
+    unsigned char bit;
+
+    while (fread(pixel, sizeof(unsigned char), 3, inputFile) == 3) {
+        if (messageIndex < MAX_MESSAGE_LENGTH) { // max size has to be calculated by checking the size of the image !! no hard coding
+            bit = (byte >> bitIndex) & 1;
+            hideBit(&pixel[0], bit);
+
+            bitIndex++;
+            if (bitIndex == 8) {
+                bitIndex = 0;
+                byte = fgetc(secretFile);
+                messageIndex++;
+            }
+        }
+
+        fwrite(pixel, sizeof(unsigned char), 3, outputFile);
+    }
+
+    fclose(inputFile);
+    fclose(secretFile);
+    fclose(outputFile);
+};
+
 int Encoding(char *filePath, char *secretMessage) {
 
     //open the file
@@ -65,7 +111,6 @@ int Encoding(char *filePath, char *secretMessage) {
         printf("Error: could not open file 1\n");
         return 1;
     }
-
 
 
     return 0;
