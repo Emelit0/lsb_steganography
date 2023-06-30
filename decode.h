5cac568a04c3bcc3cdb3b5e28395ae3da8a71d2e
Emelit0 <stollk811@gmail.com>
2023-06-30 22:33:22 +0200

    add decode header

---
+++ decode.h
@@ -0,0 +1,11 @@
+#ifndef LSB_STEGANOGRAPHY_DECODE_H
+#define LSB_STEGANOGRAPHY_DECODE_H
+
+#include "types.h"
+#include <stdio.h>
+
+/* Struct for information required for decoding
+ * the stego image to new file
+ * info about output  */
+
+#endif //LSB_STEGANOGRAPHY_DECODE_H
