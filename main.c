f91c9d26a8e986be7c3604b2fcc0218906b0557d
Emelit0 <stollk811@gmail.com>
2023-06-30 22:29:00 +0200

    new main controll flow

--- main.c
+++ main.c
@@ -12,44 +12,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "types.h"
+#include "encode.h"
+#include "decode.h"
 
-
-int readImageFile(char *filePath) {
-
-    //open the file
-    FILE *imageFile = fopen(filePath, "rb");
-    if (imageFile == NULL) {
-        printf("Error: could not open file 1\n");
-        return 1;
-    }
-
-    //read the file data
-    fseek(imageFile, 0, SEEK_END);
-    long fileSize = ftell(imageFile);
-    fseek(imageFile, 0, SEEK_SET);
-
-    //allocate memory for the file content
-    unsigned char *fileContent = malloc(fileSize);
-    if (fileContent == NULL) {
-        printf("Error: could not allocate memory for file 2\n");
-        return 1;
-    }
-
-    size_t image_data = fread(fileContent, 1, fileSize, imageFile);
-    if (image_data != fileSize) {
-        printf("Error: could not read file 3\n");
-        fclose(imageFile);
-        free(fileContent);
-        return 1;
-    }
-
-    printf("file successfully read\n");
-
-    fclose(imageFile);
-    free(fileContent);
-}
-
-
+// create a text file containing the secret message
 int createSecret() {
     char inputString[MAX_MESSAGE_LENGTH];
 
@@ -69,34 +36,58 @@ int createSecret() {
     fclose(textfile);
 }
 
-int main() {
+int main(int argc, char **argv)
+{
+            // validate input arguments
+            if (argc > 1 && argc < 8) {
+                //validate operation
 
-    createSecret();
+                //Encoding
+                if ((check_operation(argv)) == e_encode) {
+                    EncodeInfo encInfo;
 
-    FILE *destination_file;
-    char filePath[MAX_FILEPATH_LENGTH];
+                    //read file name
+                    if (validate_encode_args(argc, argv, &encInfo) == e_success) {
+                        //encode secret data
+                        if (encode(&encInfo) == e_failure) {
+                            fprintf(stderr, "Error: Encoding failed\n", "encode() function");
+                            return 1;
+                        }
+                        fclose(encInfo.fptr_src_image);
+                        fclose(encInfo.fptr_secret);
+                        fclose(encInfo.fptr_stego_image);
+                        printf("INFO: ## Encoding Done Successfully ##\n");
+                    } else {
+                        fprintf(stderr, "Error: Encoding failed\n", "validate_encode_args() function");
+                        return 1;
+                    }
+                }
+                    // Decoding
+                else if (check_operation(argv) == e_decode) {
+                    DecodeInfo decInfo;
 
-    printf("Enter the path of the image file: ");
-    fgets(filePath, MAX_FILESIZE, stdin);
-
-    printf("Enter the destination path of the manipulated image file:");
-    scanf("%s", filePath);
-
-    //need to make a copy of the selected file and store the manipulated file in a different location!!!
-    //open the destination file for writing
-    destination_file = fopen(filePath, "wb");
-    if (destination_file == NULL) {
-        printf("Error: could not open file 1\n");
-        fclose(destination_file);
-        exit(EXIT_FAILURE);
-    }
-
-    //remove trailing newline
-    filePath[strcspn(filePath, "\n")] = '\0';
-
-    printf("%s", filePath);
-
-    readImageFile(filePath);
-
-    return 0;
-}
+                    // read and store file names
+                    if (validate_decode_args(argc, argv, &decInfo) == e_success) {
+                        //decode secret data
+                        if (decode(&decInfo) == e_success) {
+                            fclose(decInfo.fptr_stego_image);
+                            fclose(decInfo.fptr_secret);
+                            printf("INFO: ## Decoding Done Successfully ##\n");
+                        } else {
+                            fprintf(stderr, "Error: Decoding failed\n", "decode() function");
+                            return 1;
+                        }
+                    } else {
+                        fprintf(stderr, "Error: Decoding failed\n", "validate_decode_args() function");
+                        return 1;
+                    }
+                } else {
+                    fprintf(stderr, "Error: Invalid operation\n");
+                    printf("Usage:\n");
+                    printf("\tEncoding: ./lsb_steg -e <.bmp file> <.txt file> <.bmp file>\n");
+                    printf("\tDecoding: ./lsb_steg -d <.bmp file> <.txt file>\n");
+                    return 1;
+                }
+            }
+            return 0;
+        }
